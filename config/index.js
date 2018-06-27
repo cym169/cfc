9b21ed17266f34626fc1ce46e872df4e64ecf298
陈明 <chenming@wident.cn>
2018-06-27 17:53:54 +0800

    最近修改

--- config/index.js
+++ config/index.js
@@ -44,7 +44,7 @@ module.exports = {
     // },
     dev: {
         env: require('./dev.env'),
-        port: 80,
+        port: 8080,
         autoOpenBrowser: true,
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
